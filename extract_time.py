b4fc507c918e83ce7e826834210cbfffedd0447e
ShaoxiongYao <williamyao@sjtu.edu.cn>
2021-07-06 17:37:51 +0800

    plot Normal_RRT and RL_RRT comparison on narrow passage statistics

--- extract_time.py
+++ extract_time.py
@@ -2,9 +2,9 @@ import numpy as np
 
 # f_list = ["/home/yixuan/EECS598_006_final_project/test_output/time_test/yixuan_21_05_24_02_19_00_horizon_5_solver_RL_RRT_env_SShape-Boxes-256Pts-SurfaceNormals-v0_cpu_True",
 # "/home/yixuan/EECS598_006_final_project/test_output/time_test/yixuan_21_05_24_02_26_01_horizon_5_solver_Normal_RRT_env_SShape-Boxes-256Pts-SurfaceNormals-v0_cpu_True"]
-f_list = ['test_output/connect_change/yixuan_21_07_05_11_01_37_horizon_5_solver_Normal_RRT_env_SShape-Boxes-1024Pts-SurfaceNormals-v0_cpu_True']
+f_list = ['test_output/connect_change/yixuan_21_07_06_01_00_58_horizon_5_solver_Normal_RRT_env_SShape-Boxes-1024Pts-SurfaceNormals-v0_cpu_True']
 
-key = "iterations:"
+key = "time"
 # key = "time"
 
 for filename in f_list:
