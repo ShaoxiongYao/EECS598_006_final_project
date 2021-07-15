d49a6d8fff877ce0096beb6cb471ed1bdead1a55
ShaoxiongYao <williamyao@sjtu.edu.cn>
2021-07-15 05:09:54 -0400

    run 5 epochs

--- run_train.sh
+++ run_train.sh
@@ -6,7 +6,9 @@ LOG_DIR=sshape_boxes_global_1024
 ENV_NAME=SShape-Boxes-1024Pts-SurfaceNormals-v0
 GPU_ID=1
 
+EPOCHS=5
+
 for SEED in 1
 do
-python -m nmp.train $ENV_NAME $LOG_DIR --gpu_id $GPU_ID --horizon 80 --seed $SEED
+python -m nmp.train $ENV_NAME $LOG_DIR --epochs $EPOCHS --gpu_id $GPU_ID --horizon 80 --seed $SEED
 done
