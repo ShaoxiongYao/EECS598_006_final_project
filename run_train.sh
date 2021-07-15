e0716a2efdf58f3df6e82e63f85ba9fce2470dd0
ShaoxiongYao <williamyao@sjtu.edu.cn>
2021-07-15 07:54:23 -0400

    change gap

--- run_train.sh
+++ run_train.sh
@@ -6,9 +6,9 @@ LOG_DIR=sshape_boxes_global_1024
 ENV_NAME=SShape-Boxes-1024Pts-SurfaceNormals-v0
 GPU_ID=1
 
-EPOCHS=5
+EPOCHS=1000
 
 for SEED in 1
 do
-python -m nmp.train --snapshot-mode gap --snapshot-gap 1 $ENV_NAME $LOG_DIR --epochs $EPOCHS --gpu_id $GPU_ID --horizon 80 --seed $SEED
+python -m nmp.train --snapshot-mode gap --snapshot-gap 20 $ENV_NAME $LOG_DIR --epochs $EPOCHS --gpu_id $GPU_ID --horizon 80 --seed $SEED
 done
