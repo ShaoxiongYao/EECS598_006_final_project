f63d647f77b764ae8ae4a6238be4c2ed17a2cf5f
ShaoxiongYao <williamyao@sjtu.edu.cn>
2021-07-20 12:58:57 +0800

    test save data

--- extract_data.py
+++ extract_data.py
@@ -2,18 +2,19 @@ import numpy as np
 
 # f_list = ["/home/yixuan/EECS598_006_final_project/test_output/time_test/yixuan_21_05_24_02_19_00_horizon_5_solver_RL_RRT_env_SShape-Boxes-256Pts-SurfaceNormals-v0_cpu_True",
 # "/home/yixuan/EECS598_006_final_project/test_output/time_test/yixuan_21_05_24_02_26_01_horizon_5_solver_Normal_RRT_env_SShape-Boxes-256Pts-SurfaceNormals-v0_cpu_True"]
-f_list = ['test_output/connect_change/yixuan_21_07_06_01_00_58_horizon_5_solver_Normal_RRT_env_SShape-Boxes-1024Pts-SurfaceNormals-v0_cpu_True']
+# f_list = ['test_output/connect_change/yixuan_21_07_06_01_00_58_horizon_5_solver_Normal_RRT_env_SShape-Boxes-1024Pts-SurfaceNormals-v0_cpu_True']
+f_list = ['test_output/it_comp/ensemble_boxes']
 
 key = "time"
 # key = "time"
 
 for filename in f_list:
-    length = []
+    data_list = []
     f = open(filename, "r")
     for line in f:
         x=line.split()
         if x[0] == key:
-            length.append(float(x[-1]))
-    print(length)
-    length_np = np.array(length)
-    print(length_np.mean())
+            data_list.append(float(x[-1]))
+    print(data_list)
+    print("mean:", np.mean(data_list))
+    print("std:", np.std(data_list))
--- test_output/it_comp/cal_std.py
+++ test_output/it_comp/cal_std.py
@@ -1,3 +1,4 @@
+import numpy as np
 import matplotlib.pyplot as plt
 
 std_list=[]
@@ -6,6 +7,8 @@ with open('debug.txt', 'r') as f:
         s = float(line)
         print(s)
         std_list.append(s)
+print("mean:", np.mean(std_list))
+print("std:", np.std(std_list))
 
 plt.hist(std_list, bins=10)
 plt.xlabel("action std")
