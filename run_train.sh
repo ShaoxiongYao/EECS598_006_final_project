cf2988ec7b931b7f528671696e713d7b0b260120
Yixuan Wang <wyx990608@sjtu.edu.cn>
2021-07-26 20:58:02 -0400

    run hardest test

--- run_train.sh
+++ run_train.sh
@@ -6,9 +6,9 @@ LOG_DIR=sshape_boxes_global_1024
 ENV_NAME=SShape-Boxes-1024Pts-SurfaceNormals-v0
 GPU_ID=1
 
-EPOCHS=1000
+EPOCHS=1001
 
-for SEED in 1
+for SEED in 2
 do
 python -m nmp.train --snapshot-mode gap --snapshot-gap 20 $ENV_NAME $LOG_DIR --epochs $EPOCHS --gpu_id $GPU_ID --horizon 80 --seed $SEED
 done
